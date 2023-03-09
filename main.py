ad89453b79449d91f0d0bc5cd10afa91826774d9
ko-arbogast <konstantin.arbogast@gmail.com>
2023-03-09 10:18:19 +0100

    Create one endpoint for all indexes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from elasticsearch import Elasticsearch
 from dotenv import load_dotenv
 import os
@@ -12,22 +12,42 @@ es = Elasticsearch(
     basic_auth=("elastic", ELASTIC_PASSWORD)
 )
 
-@gb3_search.get("/addresses")
-async def address_search(search_term: str):
-    index = "fme-addresses"
-
-    query_body = {
-        "query":{
-            "multi_match":{
-                "query":search_term,
-                "type":"cross_fields",
-                "fields":[
-                    "street",
-                    "no",
-                    "town"
-                ]
+@gb3_search.get("/search")
+async def search(indexes:str, term: str):
+    output = {}
+    results = []
+    for index in indexes.split(","):
+        if index == "fme-addresses":
+            query_body = {
+                "query":{
+                    "multi_match":{
+                        "query":term,
+                        "type":"cross_fields",
+                        "fields":[
+                            "street",
+                            "no",
+                            "town"
+                        ]
+                    }
+                }
             }
-        }
-    }
-    search_result = es.search(index=index, body=query_body)
-    return search_result
+            try:
+                search_result = es.search(index=index, body=query_body)
+                results.append(
+                    {
+                        "id": index,
+                        "data": search_result
+                    }
+                )
+            except Exception as e:
+                results.append(
+                    {
+                        "id": index,
+                        "data": e.body
+                    }
+                )
+        else:
+            raise HTTPException(status_code=404, detail=f"Search index {index} not found")
+
+    output = {"results": results}
+    return output
