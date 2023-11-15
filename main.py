a879e6f5f4f32c3b708ddf5d6e0ff8fd9d8f7dbc
Lukas Merz <lukemerz@gmail.com>
2023-11-15 13:59:51 +0100

    xx

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ gb3_search.add_middleware(
 async def search(indexes: str, term: str) -> list[SearchResult]:
     results = []
     for index in indexes.split(","):
-        print('ab')
+        print('abb')
         # https://elasticsearch-py.readthedocs.io/en/v8.10.1/api.html#elasticsearch.Elasticsearch.search
         if index.strip() == "" or "*" in index or "_all" in index:
             raise HTTPException(status_code=400, detail="Empty Index")
