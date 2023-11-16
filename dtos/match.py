5e6c3c339637146cc402cca0a03a1be1f39da820
Lukas Merz <lukemerz@gmail.com>
2023-11-16 13:14:40 +0100

    Adapt pydantic 2 changes

--- dtos/match.py
+++ dtos/match.py
@@ -1,8 +1,12 @@
 from geojson import GeoJSON
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from typing import Optional
 
+
 class Match(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
     displayString: str
     score: float = -1
     geometry: Optional[GeoJSON] = None
+
