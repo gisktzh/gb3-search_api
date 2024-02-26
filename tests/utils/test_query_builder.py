13f037ed3f6922f8a7adf03d1ce9bb27c324843a
ebp-koa <112397706+ebp-koa@users.noreply.github.com>
2024-02-26 15:01:31 +0100

    Compare pydantic models instead of dictionaries

--- tests/utils/test_query_builder.py
+++ tests/utils/test_query_builder.py
@@ -1,42 +1,43 @@
 from utils import query_builder
 from enums.fuzziness import Fuzziness
 from enums.query_operator import QueryOperator
-
+from dtos.elasticsearch_query import ElasticsearchQuery, ElasticsearchBoolQuery, ElasticsearchShouldItem,\
+    ElasticsearchMatchField
 
 def test_generates_correct_query():
     term: str = 'This is a test string'
     field_name: str = 'index_att_test'
 
-    actual: dict = query_builder.build_query(field_name, term).dict()
+    actual: ElasticsearchQuery = query_builder.build_query(field_name, term)
 
-    expected: dict = {
-        "bool": {
-            "should": [
-                {
-                    "match": {
-                        field_name: {
-                            "query": term,
-                            "boost": 1,
-                            "fuzziness": Fuzziness.AUTO,
-                            "operator": QueryOperator.AND,
-                            "analyzer": "default_search"
-                        }
+    expected = ElasticsearchQuery(
+        bool=ElasticsearchBoolQuery(
+            should=[
+                ElasticsearchShouldItem(
+                    match={
+                        field_name: ElasticsearchMatchField(
+                            query=term,
+                            boost=1,
+                            fuzziness=Fuzziness.AUTO,
+                            operator=QueryOperator.AND,
+                            analyzer="default_search"
+                        )
                     }
-                },
-                {
-                    "match": {
-                        "wordstart": {
-                            "query": term,
-                            "boost": 3,
-                            "fuzziness": Fuzziness.AUTO,
-                            "operator": QueryOperator.AND,
-                            "analyzer": "default_search"
-                        }
+                ),
+                ElasticsearchShouldItem(
+                    match={
+                        "wordstart": ElasticsearchMatchField(
+                            query=term,
+                            boost=3,
+                            fuzziness=Fuzziness.AUTO,
+                            operator=QueryOperator.AND,
+                            analyzer="default_search"
+                        )
                     }
-                }
+                )
             ],
-            "minimum_should_match": 1
-        }
-    }
+            minimum_should_match=1
+        )
+    )
 
     assert actual == expected
